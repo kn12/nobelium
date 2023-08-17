239ef8dda2c7de2d8bde3c5a5cb67418b23f563d
yinan.qin <yinan.qin@aispeech.com>
2023-08-17 20:16:59 +0800

    否则报错getServerSideProps

--- next.config.js
+++ next.config.js
@@ -15,6 +15,22 @@ module.exports = {
       'webmention.io'
     ]
   },
+  // 默认将feed重定向至 /public/rss/feed.xml
+  async redirects() {
+    return [
+      {
+        source: '/feed',
+        destination: '/rss/feed.xml',
+        permanent: true
+      }
+    ]
+  },
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    // 导出时 忽略/pages/sitemap.xml.js ， 否则报错getServerSideProps
+    const pages = { ...defaultPathMap }
+    delete pages['/sitemap.xml']
+    return pages
+  },
   eslint: {
     // dirs: ['components', 'layouts', 'lib', 'pages']
   },
